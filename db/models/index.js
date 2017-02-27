a27a5817a9f80c151e6be13e81dff46deef0869f
siteations <studer@siteations.com>
2017-02-27 16:55:36 -0600

    with associations

--- db/models/index.js
+++ db/models/index.js
@@ -18,28 +18,23 @@ const Photo = require('./photo');
 const BrewType = require('./brewType');
 const Unit = require('./unit');
 
-  // I'm assuming that the following will be added thru associations (see index).
-  // BrewID... unique brew types... each product can have one
-  // SellerID... one seller per product
-  // Photos... multiple to one product entry
-
 OAuth.belongsTo(User);
 User.hasOne(OAuth);
 
-Product.belongsTo(Seller); //sellerID on product
-Seller.hasMany(Product); //symmetrical
+Product.belongsTo(Seller, {as: 'seller'}); //sellerID on product
+Seller.hasMany(Product, {as: 'product'}); //symmetrical
 
-Seller.hasMany(Photo); //sellerID on photo
+Seller.hasMany(Photo, {as: 'seller'}); //sellerID on photo
 
 //symetrical associations?
-Product.belongsTo(BrewType); //brewID on product
-Product.belongsTo(Unit); //unitID on product
+Product.belongsTo(BrewType, {as: 'brew'}); //brewID on product
+Product.belongsTo(Unit, {as: 'unit'}); //unitID on product
 
-Product.hasMany(Photo); //productID on photo
+Product.hasMany(Photo, {as: 'product'}); //productID on photo
 
 //reviews - created from/after product by logged-in user
-Review.belongsTo(Product); //productID on review
-Review.belongsTo(User); //userID on review
+Review.belongsTo(Product, {as: 'product'}); //productID on review
+Review.belongsTo(User, {as: 'user'}); //userID on review
 
 
 module.exports = {
@@ -50,4 +45,4 @@ module.exports = {
 	BrewType,
 	Unit,
 	Review
-}
+};
