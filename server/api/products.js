a3811da3d318ce7fa113bc2897e75418cdfa63f0
siteations <studer@siteations.com>
2017-02-28 15:29:35 -0600

    everthing but the eager loading

--- server/api/products.js
+++ server/api/products.js
@@ -6,28 +6,39 @@ const Product = db.model('products')
 const {mustBeLoggedIn, forbidden,} = require('./auth.filters')
 
 module.exports = require('express').Router()
-// list all beers
+// list all beers or all beers by tag or beers by brewType
 	.get('/', (req, res, next) =>
-		Product.findAll()
-		.then()
-		.catch())
+		Product.findAll({ where: req.query })
+		.then(products => res.send(products))
+		// .then(products => res.send('hit route'))
+		.catch(next))
 // add a new beer (seller only)
 	.post('/', (req, res, next) =>
-		Product.create()
-		.then()
-		.catch())
+		Product.create(req.body)
+		.then(newProduct => res.json(newProduct))
+		.catch(next))
 // edit a beer's info (seller only)
 	.put('/:productId', (req, res, next) =>
-		Product.update()
-		.then()
-		.catch())
+		Product.update({
+			where: { id: req.params.productId }
+		})
+		.then(productUpdated => res.send(productUpdated))
+		.catch(next))
 // delete a beer (seller only)
-	.delete('/', (req, res, next) =>
-		Product.destroy()
-		.then()
+	.delete('/:productId', (req, res, next) =>
+		Product.destroy({
+			where: { id: req.params.productId }
+		})
+		.then(done =>{
+			res.send('product destroyed!')
+		})
 		.catch())
-// get info for an individual beer
+// get info for an individual beer - eager-loading gallore!
 	.get('/:productId', (req, res, next) =>
-		Product.findById()
-		.then()
-		.catch())
+		Product.findById(req.params.productId)
+		.then(product => { // EAGER ME!
+			res.send(product);
+		})
+		.catch(next))
+
+	//eager loading of sellerId, brewTypes, photos, review (userName, photos)
