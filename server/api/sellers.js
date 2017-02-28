208501fc28d5100bc7d48a606c51373709513522
David Martin <davidjmartin01@gmail.com>
2017-02-28 11:27:08 -0600

    added queries for users, sellers, and addresses

--- server/api/sellers.js
+++ server/api/sellers.js
@@ -5,28 +5,30 @@ const Seller = db.model('seller')
 const {mustBeLoggedIn, forbidden,} = require('./auth.filters')
 
 module.exports = require('express').Router()
-// list all breweries?
+// list all breweries
 	.get('/', (req, res, next) =>
-		Seller.findAll()
-		.then()
-		.catch())
+		Seller.findAll({ where: req.query })
+		.then(seller => res.json(seller))
+		.catch(next))
 // sign up new brewery
 	.post('/', (req, res, next) =>
-		Seller.create()
-		.then()
+		Seller.create(req.body)
+		.then(seller => res.json(seller))
 		.catch())
 // edit brewery info
 	.put('/:sellerId', (req, res, next) =>
-		Seller.update()
-		.then()
-		.catch())
-// delete a brewery (seller or admin only)
-	.delete('/', (req, res, next) =>
-		Seller.destroy()
-		.then()
-		.catch())
+		Seller.findById(req.params.sellerId)
+		.then(seller => seller.update(req.body))
+		.then(seller => res.json(seller))
+		.catch(next))
+// delete a brewery
+	.delete('/:sellerId', (req, res, next) =>
+		Seller.findById(req.params.sellerId)
+		.then(seller => seller.destroy())
+		.then(() => res.sendStatus(204))
+		.catch(next))
 // get info for one brewery
 	.get('/:sellerId', (req, res, next) =>
-		Seller.findById()
-		.then()
-		.catch())
+		Seller.findById(req.params.sellerId)
+		.then(seller => res.json(seller))
+		.catch(next))
