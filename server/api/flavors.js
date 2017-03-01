47907b38ce4eae331749aabdd389a56e88f3647f
siteations <studer@siteations.com>
2017-03-01 17:35:00 -0600

    flavor variable name updated. done.

--- server/api/flavors.js
+++ server/api/flavors.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const db = require('APP/db');
-const Flavors = db.model('flavors');
+const Flavor = db.model('flavors');
 
 const { mustBeLoggedIn, forbidden } = require('./auth.filters');
 
@@ -10,7 +10,7 @@ const { mustBeLoggedIn, forbidden } = require('./auth.filters');
 module.exports = require('express').Router()
 // get all flavors
 	.get('/', (req, res, next) =>
-		Flavors.findAll({
+		Flavor.findAll({
 			where: req.query//,
 			//include: [ Products ]
 		})
@@ -18,7 +18,7 @@ module.exports = require('express').Router()
 		.catch(next))
 // get info for order by ID
 	.get('/:flavorId', (req, res, next) =>
-		Flavors.findOne({
+		Flavor.findOne({
 			where: {id: req.params.flavorId},
 			include: [ Products ]
 		})
@@ -26,18 +26,18 @@ module.exports = require('express').Router()
 		.catch(next))
 // add a new flavor
 	.post('/', (req, res, next) =>
-		Flavors.create(req.body)
+		Flavor.create(req.body)
 		.then(newFlavor => res.status(201).json(newFlavor))
 		.catch(next))
 // edit an flavor info (maybe not necessary?)
 	.put('/:flavorId', (req, res, next) =>
-		Flavors.findById(req.params.flavorId)
+		Flavor.findById(req.params.flavorId)
 		.then(flavor => flavor.update(req.body))
 		.then(updatedFlavor => res.json(updatedFlavor))
 		.catch(next))
 // delete an order (hopefully won't ever want to use this)
 	.delete('/:flavorId', (req, res, next) =>
-		Flavors.findById(req.params.flavorId)
+		Flavor.findById(req.params.flavorId)
 		.then(flavor => flavor.destroy())
 		.then(() => res.sendStatus(204))
 		.catch(next))
