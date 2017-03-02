a627c365f0c88184b673fa5eb67f98d60ad6403e
Alexia Young <alexia.kwok@gmail.com>
2017-03-02 11:28:56 -0600

    adminOnly function created

--- server/api/auth.filters.js
+++ server/api/auth.filters.js
@@ -1,21 +1,28 @@
 const mustBeLoggedIn = (req, res, next) => {
   if (!req.user) {
-    return res.status(401).send('You must be logged in')
+    return res.status(401).send('You must be logged in');
   }
-  next()
-}
+  next();
+};
 
 const selfOnly = action => (req, res, next) => {
   if (req.params.id !== req.user.id) {
-    return res.status(403).send(`You can only ${action} yourself.`)
+    return res.status(403).send(`You can only ${action} yourself.`);
   }
-  next()
-}
+  next();
+};
 
 const forbidden = message => (req, res, next) => {
-  res.status(403).send(message)
-}
+  res.status(403).send(message);
+};
+
+const adminOnly = (req, res, next) => {
+  if (!req.params.isAdmin) {
+    return forbidden('You must be an admin to access.');
+  }
+  next();
+};
 
 // Feel free to add more filters here (suggested: something that keeps out non-admins)
 
-module.exports = {mustBeLoggedIn, selfOnly, forbidden }
+module.exports = { mustBeLoggedIn, selfOnly, forbidden, adminOnly };
